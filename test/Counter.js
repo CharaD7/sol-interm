c1170553fbd4fe270b89d3dd73b8723ed64ddc73
CharaD7 <jijakahn6@gmail.com>
2023-02-22 12:08:31 +0000

    Refactored code for more simplicity

--- test/Counter.js
+++ test/Counter.js
@@ -7,17 +7,40 @@ describe('Counter', () => {
   beforeEach(async () => {
     const Counter = await ethers.getContractFactory('Counter');
     counter = await Counter.deploy('My Counter', 1);
+
   })
 
   describe('Deployment', () => {
 
     it('sets the initial count', async () => {
-      expect(await counter.count()).to.equal(1)
+      expect(await counter.count()).to.equal(1);
     })
 
     it('sets the initial name', async () => {
-      expect(await counter.name()).to.equal('My Counter')
+      expect(await counter.name()).to.equal('My Counter');
     })
+  })
+
+  describe('Counting', () => {
+    let transaction;
+
+    it('increments the count', async () => {
+      transaction = await counter.increment();
+      await transaction.wait();
+
+      expect(await counter.count()).to.equal(2);
+    })
+
+    it('decrements the count', async () => {
+      transaction = await counter.decrement();
+      await transaction.wait();
+
+      expect(await counter.count()).to.equal(0);
+
+      // Cannot decrement count below 0
+      await expect(counter.decrement()).to.be.reverted;
+    })
+
   })
 
 })
